c6368544b99bf905865341336fc3fb5d0b737377
Nuno Aguiar <nmaguiar@gmail.com>
2017-02-19 21:32:29 +0000

    Corrections to fromJson

--- src/wedo/openaf/IO.java
+++ src/wedo/openaf/IO.java
@@ -133,8 +133,17 @@ public class IO extends wedo.openaf.core.IO {
 	@JSFunction
 	public static Object readFile(String filename, String encoding) throws Exception {
 		Object res = wedo.openaf.core.IO.readFile(filename, encoding);
-		
-		return wedo.openaf.AFBase.fromJson((String) res);
+	  
+                try {
+			if (encoding == null || encoding.equals("undefined")) enconding = "UTF-8";
+			JsonObject out = (new Gson()).fromJson((String) res, JsonObject.class);
+			return (new Gson()).toJson(out);
+		} catch(Exception e) {
+			SimpleLog.log(SimpleLog.logtype.DEBUG,
+					"Error parsing file: " + e.getMessage(), e);a
+			throw e; 	
+		}
+		return "";
 	}	
 
 	@JSFunction
