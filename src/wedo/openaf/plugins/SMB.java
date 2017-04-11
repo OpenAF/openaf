379fc7e376fc751d2d5d82b3ba02f3db508d43a5
Nuno Aguiar <nmaguiar@gmail.com>
2017-04-11 03:44:16 +0100

    improvements to the SMB plugin

--- src/wedo/openaf/plugins/SMB.java
+++ src/wedo/openaf/plugins/SMB.java
@@ -1,12 +1,20 @@
 package wedo.openaf.plugins;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
+import org.apache.commons.io.IOUtils;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.Undefined;
 import org.mozilla.javascript.annotations.JSConstructor;
 import org.mozilla.javascript.annotations.JSFunction;
 
@@ -14,6 +22,7 @@ import jcifs.smb.NtlmPasswordAuthentication;
 import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
+import jcifs.smb.SmbFileOutputStream;
 import wedo.openaf.AFCmdBase;
 
 /**
@@ -46,11 +55,18 @@ public class SMB extends ScriptableObject {
 	}
 	
 	@JSFunction
-	public Object listFiles() throws IOException {
+	public Object listFiles(Object path) throws IOException {
 		Scriptable no = (Scriptable) AFCmdBase.jse.newObject(AFCmdBase.jse.getGlobalscope());
 		ArrayList<Scriptable> list = new ArrayList<Scriptable>(); 
 		
-		SmbFile[] ss = smbf.listFiles();
+		SmbFile[] ss;
+		if (path != null && !(path instanceof Undefined)) {
+			SmbFile sf = new SmbFile((String) path, npa);
+			sf.connect();
+			ss = sf.listFiles();
+		} else {
+			ss = smbf.listFiles();
+		}
 		
 		for(int i = 0; i < ss.length; i++) {
 			Scriptable record = (Scriptable) AFCmdBase.jse.newObject(no);
@@ -70,10 +86,56 @@ public class SMB extends ScriptableObject {
 	}
 	
 	@JSFunction
-	public Object getFile(String aSource, String aTarget) throws MalformedURLException, SmbException, UnknownHostException {
+	public Object getFile(String aSource, String aTarget) throws IOException {
 		SmbFile f = new SmbFile(aSource, npa);
 		SmbFileInputStream sfis = new SmbFileInputStream(f);
-//		IOUtils.
-		return null;
+		FileOutputStream fw = new FileOutputStream(aTarget);
+		long res = IOUtils.copyLarge(sfis, fw);
+		IOUtils.closeQuietly(fw);
+		IOUtils.closeQuietly(sfis);
+		return res;
+	}
+	
+	@JSFunction
+	public Object putFile(String aSource, String aTarget) throws FileNotFoundException, IOException {
+		SmbFile f = new SmbFile(aTarget, npa);
+		SmbFileOutputStream sfos = new SmbFileOutputStream(f);
+		FileInputStream fis = new FileInputStream(aSource);
+		long res = IOUtils.copyLarge(fis, sfos);
+		IOUtils.closeQuietly(sfos);
+		IOUtils.closeQuietly(fis);
+		return res;
+	}
+	
+	@JSFunction
+	public Object getInputStream(String aSource) throws MalformedURLException, SmbException, UnknownHostException {
+		SmbFile f = new SmbFile(aSource, npa);
+		SmbFileInputStream sfis = new SmbFileInputStream(f);
+		return sfis;
+	}
+	
+	@JSFunction
+	public Object getFileBytes(String aSource) throws IOException {
+		SmbFile f = new SmbFile(aSource, npa);
+		SmbFileInputStream sfis = new SmbFileInputStream(f);
+		byte[] res = IOUtils.toByteArray(sfis);
+		IOUtils.closeQuietly(sfis);
+		return res;
+	}
+	
+	@JSFunction
+	public void writeFileBytes(String aTarget, Object ba, boolean append) throws IOException {
+		SmbFile f = new SmbFile(aTarget, npa);
+		SmbFileOutputStream sfos = new SmbFileOutputStream(f, append);
+		IOUtils.write((byte[]) ba, sfos);
+		IOUtils.closeQuietly(sfos);
+	}
+	
+	@JSFunction
+	public void writeFileStream(String aTarget, Object stream, boolean append) throws IOException {
+		SmbFile f = new SmbFile(aTarget, npa);
+		SmbFileOutputStream sfos = new SmbFileOutputStream(f, append);
+		IOUtils.copyLarge((InputStream) stream, sfos);
+		IOUtils.closeQuietly(sfos);
 	}
 }
