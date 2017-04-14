7fc3a4a23388ba88ec3d26a9fa2c6e3069990d6a
Nuno Aguiar <nmaguiar@gmail.com>
2017-04-14 01:10:37 +0100

    LICENSE

--- src/wedo/openaf/AFCmdBase.java
+++ src/wedo/openaf/AFCmdBase.java
@@ -15,6 +15,7 @@ import wedo.openaf.rhino.RhinoEngine;
  */
 public class AFCmdBase {
 	final public static String VERSION = "20170413";
+	final public static String LICENSE = "See license info in https://github.com/nmaguiar/openaf/LICENSE";
 	
 	public static JSEngine jse;
 	public static String afcmd = "AFCmdBase"; 
