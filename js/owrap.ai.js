4f33011292453e37a99209d30ba6260e47e5db24
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2017-10-21 17:58:04 +0100

    added ow.ai.normalize.intArray

--- js/owrap.ai.js
+++ js/owrap.ai.js
@@ -28,24 +28,55 @@ OpenWrap.ai.prototype.network = function(args) {
     }
 }
 
+/**
+ * <odoc>
+ * <key>ow.ai.network.perceptron(args) : ow.ai.network</key>
+ * Perceptron or feed-forward neural networks. There is a minimum of 3 layers (input, hidden and output) and a any
+ * nmumber of hidden layers. args = [2, 3, 1] means 2 input neurons, 3 hidden neurons and 1 output neuron; args = [2, 10, 10, 10, 10, 1]
+ * means 2 input neurons, 4 layers of 10 neurons and 1 output neuron.
+ * </odoc>
+ */
 OpenWrap.ai.prototype.network.prototype.perceptron = function(args) {
     var ar = Array.prototype.slice.call(arguments);
     ar.unshift(ow.ai.synaptic.Architect.Perceptron);
     this.__net = new (ow.ai.synaptic.Architect.Perceptron.bind.apply(ow.ai.synaptic.Architect.Perceptron, ar))();
 };
 
+/**
+ * <odoc>
+ * <key>ow.ai.network.lstm(args) : ow.ai.network</key>
+ * LSTM (Long short-term memory) are well-suited to learn from experience to classify, process and predict time series when
+ * there are very long time lags of unknown size between important events. There is a minimum of 3 layers (input, memory block (input, memory cell, forget gate, output gate), output).
+ * args = [2, 6, 1] means 2 input, 6 memory blocks, 1 output; args = [2, 4, 4, 4, 1] means 2 input neurons, 3 memory blocks and 1 output.
+ * </odoc>
+ */
 OpenWrap.ai.prototype.network.prototype.lstm = function(args) {
     var ar = Array.prototype.slice.call(arguments);
     ar.unshift(ow.ai.synaptic.Architect.LSTM);
     this.__net = new (ow.ai.synaptic.Architect.LSTM.bind.apply(ow.ai.synaptic.Architect.LSTM, ar))();    
 };
 
+/**
+ * <odoc>
+ * <key>ow.ai.network.liquid(args) : ow.ai.network</key>
+ * Liquid state machines are neural networks where neurons are randomly connected to each other. The recurrent nature of the connections turns the time
+ * varying input into a spatio-temporal pattern of activations in the network nodes. You need to provide args = [number of inputs, size of pool of neurons, number of
+ * outputs, number of random connections, number of random gates] (e.g. 2, 20, 1, 30, 10).
+ * </odoc>
+ */
 OpenWrap.ai.prototype.network.prototype.liquid = function(args) {
     var ar = Array.prototype.slice.call(arguments);
     ar.unshift(ow.ai.synaptic.Architect.Liquid);
     this.__net = new (ow.ai.synaptic.Architect.Liquid.bind.apply(ow.ai.synaptic.Architect.Liquid, ar))();       
 };
 
+/**
+ * <odoc>
+ * <key>ow.ai.network.hopfield(args) : ow.ai.network</key>
+ * Hopfield serves as a content-addressable memory remembering patterns and when feed with new patterns the network returns the most similar one from the
+ * patterns it was trained to remember. You need to provide then number of input patterns args = [ 10 ]. 
+ * </odoc>
+ */
 OpenWrap.ai.prototype.network.prototype.hopfield = function(args) {
     var ar = Array.prototype.slice.call(arguments);
     ar.unshift(ow.ai.synaptic.Architect.Hopfield);
@@ -58,7 +89,7 @@ OpenWrap.ai.prototype.network.prototype.hopfield = function(args) {
  * Trains the current network with the trainingData provided. trainingData should be an array of maps. Each 
  * map entry should have a input and output keys. Each input and output entries should be an array for the 
  * entry values and output values normalized to a decimal number between 0 and 1. Example:\
- * [{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}]\
+ * [{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}]\.
  * \
  * </odoc>
  */
@@ -105,10 +136,22 @@ OpenWrap.ai.prototype.network.prototype.fromJson = function(aJSON) {
     this.__net = ow.ai.synaptic.Network.fromJSON(aJSON);
 };
 
+/**
+ * <odoc>
+ * <key>ow.ai.network.writeFile(aFile)</key>
+ * Writes a compressed file with the map representation of the current network.
+ * </odoc>
+ */
 OpenWrap.ai.prototype.network.prototype.writeFile = function(aFile) {
     io.writeFileBytes(aFile, compress(this.toJson()));
 };
 
+/**
+ * <odoc>
+ * <key>ow.ai.network.readFile(aFile)</key>
+ * Rebuilds a network from a map stored in aFile previously with ow.ai.network.writeFile.
+ * </odoc>
+ */
 OpenWrap.ai.prototype.network.prototype.readFile = function(aFile) {
     this.fromJson(uncompress(io.readFileBytes(aFile)));
 };
@@ -125,6 +168,22 @@ OpenWrap.ai.prototype.normalize = {
         var max = isDef(aMax) ? aMax : anArray.reduce((a,b) => { return Math.max(a,b);});
         var min = isDef(aMin) ? aMin : anArray.reduce((a,b) => { return Math.min(a,b);});
 
-        return anArray.map((v) => { return (v - min)/(max - min)});
+        return anArray.map((v) => { if (v > aMax) v = aMax; if (v < aMin) v = aMin; return (v - min)/(max - min); });
+    },
+
+    /**
+     * <odoc>
+     * <key>ow.ai.normalize.intArray(anArray) : Array</key>
+     * Returns anArray where all numbers have been rounded to an integer value.
+     * </odoc>
+     */
+    intArray: function(anArray) {
+        var res = [];
+
+        for(var v in ar) {
+            res.push(Math.round(ar[v]));
+        }
+
+        return res;
     }
 };
--- tests/autoTestAll.ow.AI.js
+++ tests/autoTestAll.ow.AI.js
@@ -9,20 +9,74 @@
         ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with ow.ai perceptron 1 XOR 0");
     };
 
+    exports.testAILiquidXOR = function() {
+        ow.loadAI();
+        var nn = new ow.ai.network({ type: "liquid", args: [2, 20, 1, 30, 10]});
+        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}]);
+        ow.test.assert(Math.round(nn.get([1,0])), 1, "Problem with ow.ai liquid 1 XOR 0");
+        ow.test.assert(Math.round(nn.get([1,1])), 0, "Problem with ow.ai liquid 1 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,1])), 1, "Problem with ow.ai liquid 0 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with ow.ai liquid 1 XOR 0");   
+    };
+
+    exports.testAILSTMXOR = function() {
+        ow.loadAI();
+        var nn = new ow.ai.network({ type: "lstm", args: [2, 4, 4, 4, 1]});
+        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}], { iterations: 250000, error: 0.001 });
+        ow.test.assert(Math.round(nn.get([1,0])), 1, "Problem with ow.ai lstm 1 XOR 0");
+        ow.test.assert(Math.round(nn.get([1,1])), 0, "Problem with ow.ai lstm 1 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,1])), 1, "Problem with ow.ai lstm 0 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with ow.ai lstm 1 XOR 0");   
+    };
+
     exports.testAINetworkSave = function() {
         ow.loadAI();
         var nn = new ow.ai.network({ type: "perceptron", args: [2, 3, 1]});
         nn.train([{input: [0,0], output: [0]}, {input: [1,0], output: [0]}, {input: [0,1], output: [0]}, {input: [1,1], output :[1]}]);
-        nn.writeFile("autoTestAll.andNet.gz");
+        nn.writeFile("autoTestAll.xorNet.gz");
 
         var na = new ow.ai.network();
-        na.readFile("autoTestAll.andNet.gz");
+        na.readFile("autoTestAll.xorNet.gz");
 
         ow.test.assert(Math.round(na.get([1,0])), Math.round(nn.get([1,0])), "Problem with saved ow.ai perceptron 1 AND 0");
         ow.test.assert(Math.round(na.get([1,1])), Math.round(nn.get([1,1])), "Problem with saved ow.ai perceptron 1 AND 1");
         ow.test.assert(Math.round(na.get([0,1])), Math.round(nn.get([0,1])), "Problem with saved ow.ai perceptron 0 AND 1");
         ow.test.assert(Math.round(na.get([0,0])), Math.round(nn.get([0,0])), "Problem with saved ow.ai perceptron 1 AND 0");
 
-        af.rm("autoTestAll.andNet.gz"); 
+        af.rm("autoTestAll.xorNet.gz"); 
+    };    
+
+    exports.testAILSTMXORSave = function() {
+        ow.loadAI();
+        var nn = new ow.ai.network({ type: "lstm", args: [2, 4, 4, 4, 1]});
+        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}], { iterations: 250000, error: 0.001 });
+        nn.writeFile("autoTestAll.ai.xorNet.lstm.gz");
+
+        var na = new ow.ai.network();
+        na.readFile("autoTestAll.ai.xorNet.lstm.gz");
+
+        ow.test.assert(Math.round(nn.get([1,0])), 1, "Problem with saved ow.ai lstm 1 XOR 0");
+        ow.test.assert(Math.round(nn.get([1,1])), 0, "Problem with saved ow.ai lstm 1 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,1])), 1, "Problem with saved ow.ai lstm 0 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with saves ow.ai lstm 1 XOR 0");   
+
+        af.rm("autoTestAll.ai.xorNet.lstm.gz");
+    };
+
+    exports.testAILiquidXORSave = function() {
+        ow.loadAI();
+        var nn = new ow.ai.network({ type: "liquid", args: [2, 20, 1, 30, 10]});
+        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}]);
+        nn.writeFile("autoTestAll.ai.xorNet.liquid.gz");
+
+        var na = new ow.ai.network();
+        na.readFile("autoTestAll.ai.xorNet.liquid.gz");
+
+        ow.test.assert(Math.round(nn.get([1,0])), 1, "Problem with saved ow.ai liquid 1 XOR 0");
+        ow.test.assert(Math.round(nn.get([1,1])), 0, "Problem with saved ow.ai liquid 1 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,1])), 1, "Problem with saved ow.ai liquid 0 XOR 1");
+        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with saves ow.ai liquid 1 XOR 0");   
+
+        af.rm("autoTestAll.ai.xorNet.liquid.gz");
     };    
 })();
