d546be1fd1282e8687f49fde83463d59b1dcbd40
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2018-03-23 03:08:10 +0000

    issue #30 (improvements on AI tests)

--- tests/autoTestAll.AI.js
+++ tests/autoTestAll.AI.js
@@ -11,7 +11,7 @@
 
     exports.testAILiquidXOR = function() {
         ow.loadAI();
-        var nn = new ow.ai.network({ type: "liquid", args: [2, 20, 1, 30, 10]});
+        var nn = new ow.ai.network({ type: "liquid", args: [2, 40, 1, 2, 1]});
         nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}]);
         ow.test.assert(Math.round(nn.get([1,0])), 1, "Problem with ow.ai liquid 1 XOR 0");
         ow.test.assert(Math.round(nn.get([1,1])), 0, "Problem with ow.ai liquid 1 XOR 1");
@@ -19,14 +19,14 @@
         ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with ow.ai liquid 1 XOR 0");   
     };
 
-    exports.testAILSTMXOR = function() {
+    exports.testAILSTMAND = function() {
         ow.loadAI();
-        var nn = new ow.ai.network({ type: "lstm", args: [2, 6, 2, 1, 1]});
-        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}], { iterations: 100000, error: 0.001 });
-        ow.test.assert(Math.round(nn.get([1,0])), 1, "Problem with ow.ai lstm 1 XOR 0");
-        ow.test.assert(Math.round(nn.get([1,1])), 0, "Problem with ow.ai lstm 1 XOR 1");
-        ow.test.assert(Math.round(nn.get([0,1])), 1, "Problem with ow.ai lstm 0 XOR 1");
-        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with ow.ai lstm 1 XOR 0");   
+        var nn = new ow.ai.network({ type: "lstm", args: [2, 4, 1]});
+        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [0]}, {input: [1,0], output: [0]}, {input: [1,1], output :[1]}]);
+        ow.test.assert(Math.round(nn.get([1,0])), 0, "Problem with ow.ai lstm 1 AND 0");
+        ow.test.assert(Math.round(nn.get([1,1])), 1, "Problem with ow.ai lstm 1 AND 1");
+        ow.test.assert(Math.round(nn.get([0,1])), 0, "Problem with ow.ai lstm 0 AND 1");
+        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with ow.ai lstm 1 AND 0");   
     };
 
     exports.testAINetworkSave = function() {
@@ -46,26 +46,26 @@
         io.rm("autoTestAll.xorNet.gz"); 
     };    
 
-    exports.testAILSTMXORSave = function() {
+    exports.testAILSTMANDSave = function() {
         ow.loadAI();
-        var nn = new ow.ai.network({ type: "lstm", args: [2, 6, 2, 1, 1]});
-        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}], { iterations: 100000, error: 0.001 });
-        nn.writeFile("autoTestAll.ai.xorNet.lstm.gz");
+        var nn = new ow.ai.network({ type: "lstm", args: [2, 4, 1]});
+        nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [0]}, {input: [1,0], output: [0]}, {input: [1,1], output :[1]}]);
+        nn.writeFile("autoTestAll.ai.andNet.lstm.gz");
 
         var na = new ow.ai.network();
-        na.readFile("autoTestAll.ai.xorNet.lstm.gz");
+        na.readFile("autoTestAll.ai.andNet.lstm.gz");
 
-        ow.test.assert(Math.round(nn.get([1,0])), 1, "Problem with saved ow.ai lstm 1 XOR 0");
-        ow.test.assert(Math.round(nn.get([1,1])), 0, "Problem with saved ow.ai lstm 1 XOR 1");
-        ow.test.assert(Math.round(nn.get([0,1])), 1, "Problem with saved ow.ai lstm 0 XOR 1");
-        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with saves ow.ai lstm 1 XOR 0");   
+        ow.test.assert(Math.round(nn.get([1,0])), 0, "Problem with saved ow.ai lstm 1 AND 0");
+        ow.test.assert(Math.round(nn.get([1,1])), 1, "Problem with saved ow.ai lstm 1 AND 1");
+        ow.test.assert(Math.round(nn.get([0,1])), 0, "Problem with saved ow.ai lstm 0 AND 1");
+        ow.test.assert(Math.round(nn.get([0,0])), 0, "Problem with saves ow.ai lstm 1 AND 0");   
 
-        io.rm("autoTestAll.ai.xorNet.lstm.gz");
+        io.rm("autoTestAll.ai.andNet.lstm.gz");
     };
 
     exports.testAILiquidXORSave = function() {
         ow.loadAI();
-        var nn = new ow.ai.network({ type: "liquid", args: [2, 20, 1, 30, 10]});
+        var nn = new ow.ai.network({ type: "liquid", args: [2, 40, 1, 2, 1]});
         nn.train([{input: [0,0], output: [0]}, {input: [0,1], output: [1]}, {input: [1,0], output: [1]}, {input: [1,1], output :[0]}]);
         nn.writeFile("autoTestAll.ai.xorNet.liquid.gz");
 
