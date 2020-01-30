597b3f4bbf2124c189e6425e1c67117cfca94f69
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2020-01-30 03:22:12 +0000

    small perf improvement on classloader

--- src/openaf/OAFdCL.java
+++ src/openaf/OAFdCL.java
@@ -8,13 +8,12 @@ package openaf;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-
 import java.io.File;
 
 public class OAFdCL extends URLClassLoader {
     public static OAFdCL oafdcl = null;
 
-    public static OAFdCL getInstance(ClassLoader classLoader) {
+    final public static OAFdCL getInstance(ClassLoader classLoader) {
         if (oafdcl == null) {
             oafdcl = new OAFdCL(classLoader);
         }
@@ -24,30 +23,33 @@ public class OAFdCL extends URLClassLoader {
 
     public OAFdCL(ClassLoader classLoader) {
         super(new URL[] {}, classLoader);
-        if (oafdcl == null) oafdcl = this;
+        if (oafdcl == null) {
+            ClassLoader.registerAsParallelCapable();
+            oafdcl = this;
+        }
     }
 
-    public void appendToClassPathForInstrumentation(String path) throws MalformedURLException {
+    final public void appendToClassPathForInstrumentation(String path) throws MalformedURLException {
         super.addURL( (new File(path)).toURI().toURL() );
     }
 
     @Override
-    public void addURL(URL url) {
+    final public void addURL(URL url) {
         super.addURL(url);
     }
 
     @Override
-    public Class<?> findClass(String aName) throws ClassNotFoundException {
+    final public Class<?> findClass(String aName) throws ClassNotFoundException {
         return super.findClass(aName);
     }
 
     @Override
-    public Class<?> loadClass(String aName, boolean resolve) throws ClassNotFoundException {
+    final public Class<?> loadClass(String aName, boolean resolve) throws ClassNotFoundException {
         return super.loadClass(aName, resolve);
     }
 
     @Override
-    public URL[] getURLs() {
+    final public URL[] getURLs() {
         return super.getURLs();
     }
 }
