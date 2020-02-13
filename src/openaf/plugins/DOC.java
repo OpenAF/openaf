4018deaf95aae3a0aebfbe1ced3ea2db2794298f
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2020-02-13 04:26:38 +0000

    DOC: default headings

--- src/openaf/plugins/DOC.java
+++ src/openaf/plugins/DOC.java
@@ -6,15 +6,29 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
+import java.io.InputStream;
+import java.math.BigInteger;
+
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFStyle;
+import org.apache.poi.xwpf.usermodel.XWPFStyles;
 import org.mozilla.javascript.ScriptableObject;
-import org.mozilla.javascript.Undefined;
 import org.mozilla.javascript.annotations.JSConstructor;
 import org.mozilla.javascript.annotations.JSFunction;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTFonts;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTColor;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTDecimalNumber;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTHpsMeasure;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTOnOff;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTString;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTStyle;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STStyleType;
 
 /**
  * DOC plugin
@@ -27,6 +41,7 @@ public class DOC extends ScriptableObject {
     protected boolean read_only;
     protected File aFile;
     protected XWPFDocument doc;
+    protected Object istream;
 
     @Override
     public String getClassName() {
@@ -45,9 +60,11 @@ public class DOC extends ScriptableObject {
                 if (readOnly) {
                     // doc = DocumentFactory.create(new FileInputStream(new File((String) arg)),
                     // password);
-                    doc = new XWPFDocument(new FileInputStream(new File((String) arg)));
+                    istream = new FileInputStream(new File((String) arg));
+                    doc = new XWPFDocument((InputStream) istream);
                 } else {
-                    doc = new XWPFDocument(new FileInputStream(new File((String) arg)));
+                    istream = new FileInputStream(new File((String) arg));
+                    doc = new XWPFDocument((InputStream) istream);
                 }
             } catch (Exception e) {
 
@@ -58,7 +75,8 @@ public class DOC extends ScriptableObject {
             try {
 
             } catch (Exception e) {
-                doc = new XWPFDocument(new ByteArrayInputStream((byte[]) arg));
+                istream = new ByteArrayInputStream((byte[]) arg);
+                doc = new XWPFDocument((InputStream) istream);
             }
         }
 
@@ -70,6 +88,7 @@ public class DOC extends ScriptableObject {
     @JSFunction
     public void close() throws IOException {
         doc.close();
+        //((InputStream) istream).close();
     }
 
     @JSFunction
@@ -78,20 +97,92 @@ public class DOC extends ScriptableObject {
     }
 
     @JSFunction
-    public List<XWPFParagraph> getParagraphs() {
-        return doc.getParagraphs();
+    public Object[] getParagraphs() {
+        return doc.getParagraphs().toArray();
     }
 
     @JSFunction
-    public void writeParagraph(String aString) {
+    public void writeParagraph(String aString, String style) {
         XWPFParagraph para = doc.createParagraph();
-        XWPFRun run = para.createRun();
+        if (style != null && !style.equals("undefined")) {
+            para.setStyle(style);
+        }
 
+        XWPFRun run = para.createRun();
         run.setText(aString);
     }
 
     @JSFunction
     public void writeFile(Object file) throws FileNotFoundException, IOException {
-        doc.write(new FileOutputStream((String) file));
+        FileOutputStream out = new FileOutputStream((String) file);
+        doc.write(out);
+        out.close();
+    }
+
+    //doc.getDocument().getProperties().getCoreProperties().getCreator()
+    protected byte[] hexToBytes(String hexString) {
+        HexBinaryAdapter adapter = new HexBinaryAdapter();
+        byte[] bytes = adapter.unmarshal(hexString);
+        return bytes;
+    }
+
+    @JSFunction
+    public void addCustomHeadings() {
+        XWPFStyles styles = doc.createStyles();
+
+        this.addCustomHeadingStyle(styles, "Heading1", 1, 32, "2E74B5", "+Headings", false, false);
+        this.addCustomHeadingStyle(styles, "Heading2", 2, 26, "2E74B5", "+Headings", false, false);
+        this.addCustomHeadingStyle(styles, "Heading3", 3, 24, "1F4D78", "+Headings", false, false);
+        this.addCustomHeadingStyle(styles, "Heading4", 4, 22, "2E74B5", "+Headings", true, false);
+        this.addCustomHeadingStyle(styles, "Heading5", 5, 22, "2E74B5", "+Headings", false, false);
+    }
+
+    @JSFunction
+    public void addCustomHeadingStyle(Object styles, String strStyleId, int headingLevel, int pointSize, String hexColor, String font, boolean isItalic, boolean isBold) {
+        CTStyle ctStyle = CTStyle.Factory.newInstance();
+        ctStyle.setStyleId(strStyleId);    
+    
+        CTString styleName = CTString.Factory.newInstance();
+        styleName.setVal(strStyleId);
+        ctStyle.setName(styleName);
+    
+        CTDecimalNumber indentNumber = CTDecimalNumber.Factory.newInstance();
+        indentNumber.setVal(BigInteger.valueOf(headingLevel));
+    
+        ctStyle.setUiPriority(indentNumber);
+    
+        CTOnOff onoffnull = CTOnOff.Factory.newInstance();
+        ctStyle.setUnhideWhenUsed(onoffnull);
+        ctStyle.setQFormat(onoffnull);
+    
+        // style defines a heading of the given level
+        CTPPr ppr = CTPPr.Factory.newInstance();
+        ppr.setOutlineLvl(indentNumber);
+        ctStyle.setPPr(ppr);
+    
+        XWPFStyle style = new XWPFStyle(ctStyle);
+    
+        CTHpsMeasure size = CTHpsMeasure.Factory.newInstance();
+        size.setVal(new BigInteger(String.valueOf(pointSize)));
+        CTHpsMeasure size2 = CTHpsMeasure.Factory.newInstance();
+        size2.setVal(new BigInteger("24"));
+    
+        CTFonts fonts = CTFonts.Factory.newInstance();
+        fonts.setAscii(font);
+    
+        CTRPr rpr = CTRPr.Factory.newInstance();
+        rpr.setRFonts(fonts);
+        rpr.setSz(size);
+        rpr.setSzCs(size2);
+        if (isItalic) rpr.setI(onoffnull);
+        if (isBold) rpr.setB(onoffnull);
+    
+        CTColor color=CTColor.Factory.newInstance();
+        color.setVal(hexColor);
+        rpr.setColor(color);
+        style.getCTStyle().setRPr(rpr);
+    
+        style.setType(STStyleType.PARAGRAPH);
+        ((XWPFStyles) styles).addStyle(style);
     }
 }
