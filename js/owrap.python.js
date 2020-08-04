992ab6b21ae6775ea125a4c3a9301ab50d3e44da
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2020-08-04 05:10:56 +0100

    pyserver

--- js/owrap.python.js
+++ js/owrap.python.js
@@ -42,14 +42,13 @@ OpenWrap.python.prototype.initCode = function() {
 	}
 };
 
-OpenWrap.python.prototype.startServer = function(aPort, aFn) {
+OpenWrap.python.prototype.startServer = function(aPort, aSendPort, aFn) {
 	ow.python.cServer.inc();
-	if (isDef(this.token)) {
-		return this.token;
-	} else {
+	if (isUnDef(this.token)) {
 		this.token = md5(nowNano());
 		aFn = _$(aFn).isFunction().default((t, e) => { if (t == "error") printErr(e); });
 
+		// Receive
 		ow.loadServer();
 		if (isUnDef(aPort)) aPort = findRandomOpenPort();
 		this.port = aPort;
@@ -78,8 +77,54 @@ OpenWrap.python.prototype.startServer = function(aPort, aFn) {
 			clt.close();
 		});
 
-		return this.token;
+		// Send
+		if (isUnDef(aSendPort)) aSendPort = findRandomOpenPort();
+		this.sport = aSendPort;
+		var s = "";
+		s += "import socketserver\n";
+		s += "import json\n";
+		s += "import sys\n";
+		s += "import os\n";
+		s += "\n";
+		s += "if sys.version_info[0] == 2:\n";
+		s += "	from StringIO import StringIO\n";
+		s += "else:\n";
+		s += "	from io import StringIO\n";
+		s += "\n";
+		s += "class oafHandler(socketserver.BaseRequestHandler):\n";
+		s += "	def handle(self):\n";
+		s += "		res = ''\n";
+		s += "		while True:\n";
+		s += "			res += self.request.recv(1024).decode('utf-8')\n";
+		s += "			if str(res).endswith('}\\n'):\n";
+		s += "				break\n";
+		s += "\n";
+		s += "		mm = json.loads(res)\n";
+		s += "      if 'exit' in mm and mm['t'] == '" + this.token + "':\n";
+		s += "          os._exit(os.EX_OK)\n";
+		s += "		if 'e' in mm and mm['t'] == '" + this.token + "':\n";
+		s += "			myStdOut = StringIO()\n";
+		s += "			myStdErr = StringIO()\n";
+		s += "			sys.stdout = myStdOut\n";
+		s += "			sys.stderr = myStdErr\n";
+		s += "			try:\n";
+		s += "				exec(mm['e'])\n";
+		s += "				mm['out'] = myStdOut.getvalue()\n";
+		s += "				mm['err'] = myStdErr.getvalue()\n";
+		s += "			except:\n";
+		s += "				mm['err'] = str(sys.exc_info())\n";
+		s += "\n";
+		s += "		self.request.sendall(json.dumps(mm))\n";
+		s += "\n";
+		s += "server = socketserver.TCPServer(('127.0.0.1', " + aSendPort + "), oafHandler)\n";
+		s += "server.serve_forever()\n";
+
+		plugin("Threads");
+		var threads = new Threads();
+		threads.addSingleThread(function() { print("Starting server on " + aSendPort); sprint(af.sh("python -", s, void 0, void 0, void 0, true)); });
 	}
+
+	return this.token;
 };
 
 OpenWrap.python.prototype.stopServer = function(aPort, force) {
@@ -148,7 +193,7 @@ OpenWrap.python.prototype.execPM = function(aPythonCode, aInput, throwExceptions
 	code += aPythonCode;
 	code += "\nprint(\"" + delim + "\\n\" + json.dumps(__pm, indent=0, separators=(',',':') ))\n";
 
-	var res = af.sh("python -", code, void 0, void 0, void 0, true);
+	var res = af.sh(this.python + " -", code, void 0, void 0, void 0, true);
 	var rres = [];
 	if (res.stdout.indexOf(delim) >= 0) {
 		rres = res.stdout.split(new RegExp("^" + delim + "\r?\n", "mg"));
@@ -193,7 +238,7 @@ OpenWrap.python.prototype.exec = function(aPythonCode, aInput, aOutputArray, thr
 
 	code += "\nprint(\"" + delim + "\\n\" + json.dumps({ " + Object.keys(aOutputArray).map(k => "\"" + aOutputArray[k] + "\": " + aOutputArray[k]).join(", ") + " }, indent=0, separators=(',',':') ))\n";
 
-	var res = af.sh("python -", code, void 0, void 0, void 0, true);
+	var res = af.sh(this.python + " -", code, void 0, void 0, void 0, true);
 	var rres = [];
 	if (res.stdout.indexOf(delim) >= 0) {
 		rres = res.stdout.split(new RegExp("^" + delim + "\r?\n", "mg"));
@@ -205,6 +250,41 @@ OpenWrap.python.prototype.exec = function(aPythonCode, aInput, aOutputArray, thr
 			throw "python: " + String(res.stderr);
 		} else {
 			printErr(res.stderr);
+		}
+	}
+	return jsonParse(rres[1], true);
+};
+
+OpenWrap.python.prototype.iexec = function(aPythonCode, aInput, aOutputArray, throwExceptions) {
+	if (this.version < 0) throw "Appropriate Python version not found. Please setPython to a python version 2 or version 3 command-line interpreter.";
+
+	_$(aPythonCode, "python code").isString().$_();
+
+	aInput = _$(aInput, "input").isMap().default({});
+	aOutputArray = _$(aOutputArray, "output").isArray().default([]);
+	throwExceptions = _$(throwExceptions, "throwExceptions").isBoolean().default(false);
+
+	var code = this.initCode(), delim = nowNano();
+	code += "import json\n";
+	Object.keys(aInput).map(k => {
+		if (isDef(aInput[k])) code += k + " = json.loads('" + stringify(aInput[k], void 0, "" ).replace(/\'/g, "\\'").replace(/\\n/g, "\\\n") + "')\n";
+	});
+	code += aPythonCode;
+
+	code += "\nprint(\"" + delim + "\\n\" + json.dumps({ " + Object.keys(aOutputArray).map(k => "\"" + aOutputArray[k] + "\": " + aOutputArray[k]).join(", ") + " }, indent=0, separators=(',',':') ))\n";
+
+	var res = jsonParse(ow.ojb.socket.string2string("127.0.0.1", this.sport, stringify({ e: code }, void 0, "")+"\n"));
+	var rres = [];
+	if (res.out.indexOf(delim) >= 0) {
+		rres = res.out.split(new RegExp("^" + delim + "\r?\n", "mg"));
+	}
+	if (isDef(rres[0]) && rres[0] != "") print(rres[0]);
+	if (isDef(res.err) && res.err != "") {
+		if (throwExceptions) {
+			printErr(res.err);
+			throw "python: " + String(res.err);
+		} else {
+			printErr(res.err);
 		}
 	}
 	return jsonParse(rres[1], true);
