26233b9f0ce90f2cbcf1861e28deda1e0265d84c
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2021-05-25 15:54:19 +0100

    cleanup

--- e.py
+++
@@ -1,5 +0,0 @@
-version = _('getVersion()')
-print("Python running on OpenAF version " + version)
-
-joke = _('$rest().get("https://api.chucknorris.io/jokes/random")')
-print(joke['value'])
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git config --global user.email "$1@users.noreply.github.com"
-wget https://openaf.io/unix64/nightly/install.sh
