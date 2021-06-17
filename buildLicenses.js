20f859b75fe14d346364ddb64df3422f0ab20265
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2021-06-17 10:56:50 +0100

    versions update

--- buildLicenses.js
+++ buildLicenses.js
@@ -8,13 +8,13 @@ for(let i in ls.external) {
     if (ls.external[i].license.match(/^https?:/i)) {
         ls.external[i].license = h.exec(ls.external[i].license).response;
     }
-    ls.external[i].path = ls.external[i].path.join(", ");
-
+    ls.external[i].ppath = ls.external[i].path.join(", ");
+    
     lines += templify("\
 Third-party name      : {{description}}\n\
 Version               : {{version}}\n\
 Changed from original : {{#if changes}}Yes{{else}}No{{/if}}\n\
-Location in openaf.jar: {{path}}\n\
+Location in openaf.jar: {{ppath}}\n\
 License               : \n\n{{license}}\n\
 -----------------------\n", ls.external[i]);
 }
