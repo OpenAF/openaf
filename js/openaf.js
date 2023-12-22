4e8e442f2d7d880de5754e607c5d61a48ee3d4fb
nmaguiar <nmaguiar@users.noreply.github.com>
2023-12-22 12:36:03 +0000

    removing $sh.wd (use $sh.pwd)

--- js/openaf.js
+++ js/openaf.js
@@ -10438,17 +10438,6 @@ const $sh = function(aString, aIn) {
 
 	/**
 	 * <odoc>
-	 * <key>$sh.wd(aWd) : $sh</key>
-	 * When executing aCmd (with .exec) use aWd as the current working directory.
-	 * </odoc>
-	 */
-    __sh.prototype.wd = function(aWd) {
-        this.wd = aWd
-        return this
-    }
-
-	/**
-	 * <odoc>
 	 * <key>$sh.cb(aCallbackFunc) : $sh</key>
 	 * When executing aCmd (with .get) use aCallbackFunc function.
 	 * </odoc>
