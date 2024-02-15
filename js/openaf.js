2631cc195bcbcdc1f8eec24c1b579b59ecba5327
nmaguiar <nmaguiar@users.noreply.github.com>
2024-02-15 23:14:03 +0000

    $path: +to/from_slon, to/from_json, to/from_yaml

--- js/openaf.js
+++ js/openaf.js
@@ -4306,7 +4306,7 @@ var $from = function(a) {
  *   $path(arr, "a[?contains(@, 'b') == `true`]")\
  * \
  * [OpenAF custom functions]: \
- *   a2m(arrFields, arrValues), a4m(arr, 'key', dontRemove), m2a(arrFields, obj), m4a(obj, 'key'), count_by(arr, 'field'), format(x, 'format'), formatn(x, 'format'), group(arr, 'field'), group_by(arr, 'field1,field2'), unique(arr), to_map(arr, 'field'), to_date(x), to_isoDate(x), flat_map(x), search_keys(arr, 'text'), search_values(arr, 'text'), delete(map, 'field'), substring(a, ini, end), template(a, 'template'), templateF(x, 'template'), to_bytesAbbr(x), to_numAbbr(x), from_bytesAbbr(x), from_siAbbr(x), from_timeAbbr(x), timeago(x), from_ms(x, 'format'), replace(x, 're', 'flags', 'replaceText'), split(x, 'sep'), trim(x), index_of(x, 'search'), last_index_of(x, 'search'), lower_case(x), upper_case(x), concat(x, y), match(x, 're', 'flags'), amerge(x, y)\
+ *   a2m(arrFields, arrValues), a4m(arr, 'key', dontRemove), m2a(arrFields, obj), m4a(obj, 'key'), count_by(arr, 'field'), format(x, 'format'), formatn(x, 'format'), group(arr, 'field'), group_by(arr, 'field1,field2'), unique(arr), to_map(arr, 'field'), to_date(x), to_isoDate(x), flat_map(x), search_keys(arr, 'text'), search_values(arr, 'text'), delete(map, 'field'), substring(a, ini, end), template(a, 'template'), templateF(x, 'template'), to_bytesAbbr(x), to_numAbbr(x), from_bytesAbbr(x), from_siAbbr(x), from_timeAbbr(x), timeago(x), from_ms(x, 'format'), replace(x, 're', 'flags', 'replaceText'), split(x, 'sep'), trim(x), index_of(x, 'search'), last_index_of(x, 'search'), lower_case(x), upper_case(x), concat(x, y), match(x, 're', 'flags'), amerge(x, y), to_slon(x), from_slon(x), to_json(x), from_json(x, str), to_yaml(x, isMultiDoc), from_yaml(x)\
  * \
  * Custom functions:\
  *   $path(2, "example(@)", { example: { _func: (a) => { return Number(a) + 10; }, _signature: [ { types: [ $path().number ] } ] } });\
@@ -4466,6 +4466,30 @@ const $path = function(aObj, aPath, customFunctions) {
 		amerge: {
 			_func: ar => merge(ar[0], ar[1]),
 			_signature: [ { types: [ jmespath.types.array, jmespath.types.object ] }, { types: [ jmespath.types.array, jmespath.types.object ] } ]
+		},
+		to_slon: {
+			_func: ar => af.toSLON(ar[0]),
+			_signature: [ { types: [ jmespath.types.any ] } ]
+		},
+		from_slon: {
+			_func: ar => af.fromSLON(ar[0]),
+			_signature: [ { types: [ jmespath.types.string ] } ]
+		},
+		to_json: {
+			_func: ar => jsonParse(ar[0], __, __, true),
+			_signature: [ { types: [ jmespath.types.string ] } ]
+		},
+		from_json: {
+			_func: ar => stringify(ar[0], __, ar[1]),
+			_signature: [ { types: [ jmespath.types.any ] }, { types: [ jmespath.types.string ] } ]
+		},
+		to_yaml: {
+			_func: ar => af.toYAML(ar[0], ar[1]),
+			_signature: [ { types: [ jmespath.types.any ] }, { types: [ jmespath.types.boolean ] } ]
+		},
+		from_yaml: {
+			_func: ar => af.fromYAML(ar[0]),
+			_signature: [ { types: [ jmespath.types.string ] } ]
 		}
 	}, customFunctions)
 
