e436b816e14f6f1b1fe7dfe55213cab5ccff88d2
nmaguiar <nmaguiar@users.noreply.github.com>
2024-02-17 20:24:36 +0000

    $path: add arithmetic functions to custom functions

--- js/openaf.js
+++ js/openaf.js
@@ -4306,7 +4306,8 @@ var $from = function(a) {
  *   $path(arr, "a[?contains(@, 'b') == `true`]")\
  * \
  * [OpenAF custom functions]: \
- *   a2m(arrFields, arrValues), a4m(arr, 'key', dontRemove), m2a(arrFields, obj), m4a(obj, 'key'), count_by(arr, 'field'), format(x, 'format'), formatn(x, 'format'), group(arr, 'field'), group_by(arr, 'field1,field2'), unique(arr), to_map(arr, 'field'), to_date(x), to_isoDate(x), flat_map(x), search_keys(arr, 'text'), search_values(arr, 'text'), delete(map, 'field'), substring(a, ini, end), template(a, 'template'), templateF(x, 'template'), to_bytesAbbr(x), to_numAbbr(x), from_bytesAbbr(x), from_siAbbr(x), from_timeAbbr(x), timeago(x), from_ms(x, 'format'), replace(x, 're', 'flags', 'replaceText'), split(x, 'sep'), trim(x), index_of(x, 'search'), last_index_of(x, 'search'), lower_case(x), upper_case(x), concat(x, y), match(x, 're', 'flags'), amerge(x, y), to_slon(x), from_slon(x), to_json(x), from_json(x, str), to_yaml(x, isMultiDoc), from_yaml(x), trim(x), nvl(x, v)\
+ *   a2m(arrFields, arrValues), a4m(arr, 'key', dontRemove), m2a(arrFields, obj), m4a(obj, 'key'), count_by(arr, 'field'), format(x, 'format'), formatn(x, 'format'), group(arr, 'field'), group_by(arr, 'field1,field2'), unique(arr), to_map(arr, 'field'), to_date(x), to_isoDate(x), flat_map(x), search_keys(arr, 'text'), search_values(arr, 'text'), delete(map, 'field'), substring(a, ini, end), template(a, 'template'), templateF(x, 'template'), to_bytesAbbr(x), to_numAbbr(x), from_bytesAbbr(x), from_siAbbr(x), from_timeAbbr(x), timeago(x), from_ms(x, 'format'), replace(x, 're', 'flags', 'replaceText'), split(x, 'sep'), trim(x), index_of(x, 'search'), last_index_of(x, 'search'), lower_case(x), upper_case(x), concat(x, y), match(x, 're', 'flags'), amerge(x, y), to_slon(x), from_slon(x), to_json(x), from_json(x, str), to_yaml(x, isMultiDoc), from_yaml(x), trim(x), nvl(x, v)
+ * add(x, y), sub(x, y), mul(x, y), div(x, y), mod(x, y)\
  * \
  * Custom functions:\
  *   $path(2, "example(@)", { example: { _func: (a) => { return Number(a) + 10; }, _signature: [ { types: [ $path().number ] } ] } });\
@@ -4498,6 +4499,26 @@ const $path = function(aObj, aPath, customFunctions) {
 		nvl: {
 			_func: ar => (isNull(ar[0]) || isUnDef(ar[0]) ? ar[1] : ar[0]),
 			_signature: [ { types: [ jmespath.types.any ] }, { types: [ jmespath.types.any ] } ]
+		},
+		add: {
+			_func: ar => ar[0] + ar[1],
+			_signature: [ { types: [ jmespath.types.number ] }, { types: [ jmespath.types.number ] } ]
+		},
+		sub: {
+			_func: ar => ar[0] - ar[1],
+			_signature: [ { types: [ jmespath.types.number ] }, { types: [ jmespath.types.number ] } ]
+		},
+		mul: {
+			_func: ar => ar[0] * ar[1],
+			_signature: [ { types: [ jmespath.types.number ] }, { types: [ jmespath.types.number ] } ]
+		},
+		div: {
+			_func: ar => ar[0] / ar[1],
+			_signature: [ { types: [ jmespath.types.number ] }, { types: [ jmespath.types.number ] } ]
+		},
+		mod: {
+			_func: ar => ar[0] % ar[1],
+			_signature: [ { types: [ jmespath.types.number ] }, { types: [ jmespath.types.number ] } ]
 		}
 	}, customFunctions)
 
@@ -11501,7 +11522,11 @@ const $output = function(aObj, args, aFunc, shouldReturn) {
 		if (shouldReturn)
 			return _s
 		else {
-			if (pause) ow.format.string.pauseString(_s); else print(_s)
+			if (isUnDef(_s)) {
+				print("")
+			} else {
+				if (pause) ow.format.string.pauseString(_s); else print(_s)
+			}
 		}
 	}
 
@@ -11633,6 +11658,7 @@ const $output = function(aObj, args, aFunc, shouldReturn) {
 				aFunc(res)
 			}
 	}
+	return fnP(__)
 }
 const $o = $output
 
