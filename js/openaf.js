35b49a4b3fb077fd2daa8a97277720a0d4949f99
nmaguiar <nmaguiar@users.noreply.github.com>
2024-06-25 17:35:13 +0100

    printTree: better handling of nulls

--- js/openaf.js
+++ js/openaf.js
@@ -899,8 +899,8 @@ const printTable = function(anArrayOfEntries, aWidthLimit, displayCount, useAnsi
 			shouldBand = false
 			var _found = false
 			anArrayOfIdxs.reduce((acc, pos, _ii) => {
-				if (!_found && acc >= ii && ii < pos) {
-					shouldBand = anArrayOfIdxs.indexOf(pos) % 2 != 0
+				if (!_found && acc > ii && ii <= pos) {
+					shouldBand = anArrayOfIdxs.indexOf(pos) % 2 == 0
 					_found = true
 				}
 				return pos
@@ -1079,6 +1079,7 @@ const printTree = function(_aM, _aWidth, _aOptions, _aPrefix, _isSub) {
     var _clr = __, _ac = __, _al = __
     if (!_aOptions.noansi) {
         _dt = aO => {
+			if (null == aO) return "null"
             if (isJavaObject(aO) || isJavaClass(aO)) return "java"
             if (isUnDef(aO)) return "undefined"
             if (isBoolean(aO)) return "boolean"
@@ -1094,6 +1095,7 @@ const printTree = function(_aM, _aWidth, _aOptions, _aPrefix, _isSub) {
             let result
             let dt = _dt(aO)
             switch(dt) {
+			case "null"   : result = [_ac(__colorFormat.default, "null"), _acr()].join(""); break
             case "number" : result = [_ac(__colorFormat.number, String(aO)), _acr()].join(""); break
             case "string" : result = [_ac(__colorFormat.string, String(aO)), _acr()].join(""); break
             case "boolean": result = [_ac(__colorFormat.boolean, String(aO)), _acr()].join(""); break
@@ -1233,11 +1235,11 @@ const printTree = function(_aM, _aWidth, _aOptions, _aPrefix, _isSub) {
       
 		var _out = {}
         //parallel4Array(aMKeys.map((k, i) => ({ k: k, i: i })), _v => {
-		pForEach(aMKeys, (k, i) => {
+	pForEach(aMKeys, (k, i) => {
 			//try {
 			//let k = _v.k, i = _v.i
             let v = aOptions.fullValSize ? _getCache.get(k) : _get(k, aM[k]), lv = _al(v)
-
+			
             let ksizeOrAlKPlusSline = (isDef(ksize) ? ksize : _al(k)) + slines
             let vsizeOrLvPlusSline = (isDef(vsize) ? vsize : lv) + slines
             let aPrefix2 = _ac(__colorFormat.tree.lines, (i < (size-1) ? line : " ") + " ".repeat(ksizeOrAlKPlusSline))
@@ -1249,7 +1251,7 @@ const printTree = function(_aM, _aWidth, _aOptions, _aPrefix, _isSub) {
             let reset = (i == 0 || i > 0) ? __ansiColorCache["RESET"] : ""
             let suffix
 
-            if (isDef(aM[k]) && (isMap(aM[k]) || Array.isArray(aM[k]))) {
+            if (isDef(aM[k]) && aM[k] != null && (isMap(aM[k]) || Array.isArray(aM[k]))) {
                 suffix = _pt(aM[k], aWidth, aOptions, [aPrefix, _ac(__colorFormat.tree.lines, [(i < (size-1) ? line : " "), " ".repeat(vsizeOrLvPlusSline)].join(""))].join(""), true)
             }
 
@@ -6801,82 +6803,82 @@ const getFromZip = function(aZipFile, aResource, isBy, encoding, notInMemory) {
 
 //Production steps of ECMA-262, Edition 6, 22.1.2.1
 //Reference: https://people.mozilla.org/~jorendorff/es6-draft.html#sec-array.from
-if (!Array.from) {
-	Array.from = (function () {
-		var toStr = Object.prototype.toString;
-		var isCallable = function (fn) {
-			return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
-		};
-		var toInteger = function (value) {
-			var number = Number(value);
-			if (isNaN(number)) { return 0; }
-			if (number === 0 || !isFinite(number)) { return number; }
-			return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
-		};
-		var maxSafeInteger = Math.pow(2, 53) - 1;
-		var toLength = function (value) {
-			var len = toInteger(value);
-			return Math.min(Math.max(len, 0), maxSafeInteger);
-		};
+// if (!Array.from) {
+// 	Array.from = (function () {
+// 		var toStr = Object.prototype.toString;
+// 		var isCallable = function (fn) {
+// 			return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+// 		};
+// 		var toInteger = function (value) {
+// 			var number = Number(value);
+// 			if (isNaN(number)) { return 0; }
+// 			if (number === 0 || !isFinite(number)) { return number; }
+// 			return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+// 		};
+// 		var maxSafeInteger = Math.pow(2, 53) - 1;
+// 		var toLength = function (value) {
+// 			var len = toInteger(value);
+// 			return Math.min(Math.max(len, 0), maxSafeInteger);
+// 		};
 
-		// The length property of the from method is 1.
-		return function from(arrayLike/*, mapFn, thisArg */) {
-			// 1. Let C be the this value.
-			var C = this;
+// 		// The length property of the from method is 1.
+// 		return function from(arrayLike/*, mapFn, thisArg */) {
+// 			// 1. Let C be the this value.
+// 			var C = this;
 
-			// 2. Let items be ToObject(arrayLike).
-			var items = Object(arrayLike);
+// 			// 2. Let items be ToObject(arrayLike).
+// 			var items = Object(arrayLike);
 
-			// 3. ReturnIfAbrupt(items).
-			if (arrayLike == null) {
-				throw new TypeError("Array.from requires an array-like object - not null or undefined");
-			}
+// 			// 3. ReturnIfAbrupt(items).
+// 			if (arrayLike == null) {
+// 				throw new TypeError("Array.from requires an array-like object - not null or undefined");
+// 			}
 
-			// 4. If mapfn is undefined, then let mapping be false.
-			var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
-			var T;
-			if (typeof mapFn !== 'undefined') {
-				// 5. else      
-				// 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
-				if (!isCallable(mapFn)) {
-					throw new TypeError('Array.from: when provided, the second argument must be a function');
-				}
+// 			// 4. If mapfn is undefined, then let mapping be false.
+// 			var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+// 			var T;
+// 			if (typeof mapFn !== 'undefined') {
+// 				// 5. else      
+// 				// 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+// 				if (!isCallable(mapFn)) {
+// 					throw new TypeError('Array.from: when provided, the second argument must be a function');
+// 				}
 
-				// 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
-				if (arguments.length > 2) {
-					T = arguments[2];
-				}
-			}
+// 				// 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+// 				if (arguments.length > 2) {
+// 					T = arguments[2];
+// 				}
+// 			}
 
-			// 10. Let lenValue be Get(items, "length").
-			// 11. Let len be ToLength(lenValue).
-			var len = toLength(items.length);
+// 			// 10. Let lenValue be Get(items, "length").
+// 			// 11. Let len be ToLength(lenValue).
+// 			var len = toLength(items.length);
 
-			// 13. If IsConstructor(C) is true, then
-			// 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
-			// 14. a. Else, Let A be ArrayCreate(len).
-			var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+// 			// 13. If IsConstructor(C) is true, then
+// 			// 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
+// 			// 14. a. Else, Let A be ArrayCreate(len).
+// 			var A = isCallable(C) ? Object(new C(len)) : new Array(len);
 
-			// 16. Let k be 0.
-			var k = 0;
-			// 17. Repeat, while k < len… (also steps a - h)
-			var kValue;
-			while (k < len) {
-				kValue = items[k];
-				if (mapFn) {
-					A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
-				} else {
-					A[k] = kValue;
-				}
-				k += 1;
-			}
-			// 18. Let putStatus be Put(A, "length", len, true).
-			A.length = len;
-			// 20. Return A.
-			return A;
-		};
-	}());
-}
+// 			// 16. Let k be 0.
+// 			var k = 0;
+// 			// 17. Repeat, while k < len… (also steps a - h)
+// 			var kValue;
+// 			while (k < len) {
+// 				kValue = items[k];
+// 				if (mapFn) {
+// 					A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+// 				} else {
+// 					A[k] = kValue;
+// 				}
+// 				k += 1;
+// 			}
+// 			// 18. Let putStatus be Put(A, "length", len, true).
+// 			A.length = len;
+// 			// 20. Return A.
+// 			return A;
+// 		};
+// 	}());
+// }
 
 /**
  * <odoc>
