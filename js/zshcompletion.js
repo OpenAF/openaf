36a8a54df82c9e57a26f9308e24c5a32a3c3e98c
nmaguiar <nmaguiar@users.noreply.github.com>
2024-08-30 02:49:08 +0100

    refactor: Update completion scripts for opack in zsh and bash

--- js/zshcompletion.js
+++ js/zshcompletion.js
@@ -12,7 +12,7 @@ case "ojob"  :
     var shell1 = io.readFileString(getOpenAFJar() + "::complete/completion_zsh.hbs").replace(/{{request}}/g, "sh {{home}}/.openaf_completion_{{tool}}.sh").replace(/{{tool}}/g, "ojob").replace(/{{home}}/g, homeDir)
     var fs = io.createTempFile("ojob", "")
     io.writeFileString(fs, io.readFileString(getOpenAFJar() + "::complete/completion_ojob.yaml"))
-    var ojobio = $rest().get("https://ojob.io/index.json").init.l.map(r => r.replace(/^https:\/\/(.+)\.yaml/,"$1"))
+    var ojobio = $rest().get("https://ojob.io/index.json").init.l.map(r => r.replace(/^https:\/\/(.+)\.(yaml|json|yml|sh|bat)$/,"$1"))
     var s = "\n" + ojobio.map(r => "- name: " + r).join("\n")
     io.writeFileString(fs, s, __, true)
 
@@ -35,5 +35,4 @@ case "opack" :
 default: 
 }
 
-io.writeFileString("done.txt", "done5")
 exit(0, true)
