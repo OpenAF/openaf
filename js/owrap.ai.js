5a641aa420c3d48c7b8c8676b547b361971c29e4
nmaguiar <nmaguiar@users.noreply.github.com>
2025-07-11 22:17:16 +0100

    fix: update system_instruction mapping to handle message structure in GPT types

--- js/owrap.ai.js
+++ js/owrap.ai.js
@@ -436,7 +436,7 @@ OpenWrap.ai.prototype.__gpttypes = {
                     msgs = aPrompt.map(c => isMap(c) ? c : { role: "user", parts: [ { text: c } ] })
                  
                     var body = {
-                        system_instruction: { parts: _r.conversation.filter(r => isDef(r.role) && r.role == "system").map(r => ({ text: r.text }) ) },
+                        system_instruction: { parts: _r.conversation.filter(r => isDef(r.role) && r.role == "system").map(r => isMap(r) ? r : { role: r.role, parts: [ { text: r.content } ]} ) },
                         contents: msgs,
                         generationConfig: {
                             temperature: aTemperature
